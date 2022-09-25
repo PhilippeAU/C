57756d1952daf996dcc1ae63dc98fff8fa5d989f
Philippe <philippeaugusto96@gmail.com>
2022-09-25 01:45:32 -0300

    2

---
+++ dfgsdf.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+	
+	double v[1.1,2.2,3.3];
+	pritnf("O valor de *pv é %f\n", *pv);
+	*pv = v*pv2;
+	pv2=pv+2;
+	pritnf("O valor de *pv é %f\n",*pv2);
+	pv2--;
+    pritnf("O valor de *pv é %f\n",*pv2);
+	
+	
+}
